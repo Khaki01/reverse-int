039ad3a72cc51af9a55e509ef696f5aa5b7dc553
Khaki01 <kh.mukashev@gmail.com>
2021-02-09 22:58:03 +0600

    feat: add reverse int algo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if(n<0) n*=-1;
+    let res = 0;
+    while(n>0){
+        res = res*10 + n%10;
+        n = Math.floor(n/10);
+    }
+    return res;
 }
